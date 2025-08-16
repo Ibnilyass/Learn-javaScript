13e80795d94f49dbf4b3a4009de5261f26e2d53f
Abubakar Ilyas <abubakarilyas2837@gmail.com>
2025-08-16 02:28:38 +0000

    no message

---
+++ 7_string_to_number.js
@@ -0,0 +1,29 @@
+console.log("hello");
+
+let value = 66;
+let negvalue = -(value);
+console.log(value);
+
+console.log(2+2);
+
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+
+let str1 = "ABU";
+let str2 = " AMI";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1"+3);
+console.log(1+"3");
+
+console.log("hy");
+
+console.log(true);
+console.log(+true);
+console.log(-"");
+
